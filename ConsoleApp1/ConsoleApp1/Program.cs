3fd24a5d304ee00fbf06354c7620e26532c1716a
strongu01 <arturstrongowski@wp.pl>
2017-06-02 09:24:38 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using Aplikacja1.Figury;
+using Aplikacja1.Figury;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +9,15 @@ namespace Aplikacja1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Pozycja p = new Pozycja("B5");
             Pozycja p2 = new Pozycja("D4");
-            Figura f = new Pionek(p, Kolor.KOLOR_BIALY);
+            Figura f = new Pionek(p,Kolor.KOLOR_BIALY);
             Console.WriteLine(f.CzyMoznaPrzesunac(p2));
-
+             Szachownica sz=new Szachownica();
+             sz['A', 1] = "test";
+             Console.WriteLine(sz['A',1]);
 
         }
     }
